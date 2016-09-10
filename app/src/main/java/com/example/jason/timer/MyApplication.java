f6656e68e76a33ae8270b066d195b57058f44968
jason-ele <jason.wang56837@gmail.com>
2016-09-11 01:50:51 +0800

    init Timer project

---
+++ app/src/main/java/com/example/jason/timer/MyApplication.java
@@ -0,0 +1,7 @@
+package com.example.jason.timer;
+
+/**
+ * Created by jason on 16-9-11.
+ */
+public class MyApplication {
+}
---
+++ app/src/main/java/com/example/jason/timer/com/example/jason/timer/utils/HttpHelper.java
@@ -0,0 +1,7 @@
+package com.example.jason.timer.com.example.jason.timer.utils;
+
+/**
+ * Created by jason on 16-9-11.
+ */
+public class HttpHelper {
+}
